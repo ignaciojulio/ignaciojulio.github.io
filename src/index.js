7acab3f98a849c3948bc6c8b86e0bc7103fe3b9a
Ignacio Javier Julio Posada <ignacio9876.ijjp@gmail.com>
2022-12-10 05:42:43 -0500

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Template from './templates/Template.js';
 console.log('hola');
 
-(async function App() {
-  const main = null || document.getElementById('main');
-  main.innerHTML = await Template();
+(async function header() {
+  const header = null || document.querySelector('header');
+  header.innerHTML = await Template();
 })();
--- src/templates/Template.js
+++ src/templates/Template.js
@@ -1,42 +1,25 @@
-import getData from '../utils/getData.js';
-
 const Template = async () => {
-  const data = await getData();
-  const view = `
-    <div class="About">
-      <div class="card">
-        <div class="card_details">
-          <div class="card_photo center circle">
-            <img src="${data.picture.large}" alt="${data.name.first}">
-            <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" style="enable-background:new -580 439 577.9 194;"
-              xml:space="preserve">
-              <circle cx="50" cy="50" r="40" />
-            </svg>
-          </div>
-          <p class="card_title">Hi, My name is</p>
-          <p class="card_value">${data.name.first} ${data.name.last}</p>
-        </div>
-        <div class="card_userdata">
-          <ul>
-            <li>${data.email}</li>
-            <li>${data.location.country}</li>
-          </ul>
-        </div>
-        <div class="card_social">
-          <a href="https://twitter.com/gndx">
-            <img src="../src/assets/images/twitter.png" />
-          </a>
-          <a href="https://github.com/gndx">
-            <img src="../src/assets/images/github.png" />
-          </a>
-          <a href="https://instagram.com/gndx">
-            <img src="../src/assets/images/instagram.png" />
-          </a>
-        </div>
-      </div>
-    </div>
+    const view = `
+            <figure>
+                <img src="./assets/icon/logo_white.svg" alt="logo">
+                <h1>Ignacio</h1>
+            </figure>
+            <nav>
+                <ul>
+                    <li><a href="#">#Inicio</a></li>
+                    <li><a href="#">#Trabajos</a></li>
+                    <li><a href="#">#Hacerca de mi</a></li>
+                    <li><a href="#">#Contactos</a></li>
+                    <div>
+                        <select name="lenguajes" id="lang">
+                        <option value="ES">ES</option>
+                        <option value="EN">EN</option>
+                        <option value="日本語">日本語</option>
+                    </div>
+                </ul>
+            </nav>
   `;
-  return view;
+    return view;
 };
 
 export default Template;

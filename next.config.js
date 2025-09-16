211d7fdf2a823e3476f5c707725c40ba0c691426
Ignacio Javier Julio Posada <67277216+ignaciojulio@users.noreply.github.com>
2025-09-16 01:08:45 -0500

    config nextConfig

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+  
+  // Asegura que las URLs terminen en barra inclinada
   trailingSlash: true,
 }
+
+module.exports = nextConfig;
